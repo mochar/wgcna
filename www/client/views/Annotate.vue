ff8c593945647dbb6abcee508720c99c6e1ca60f
DanielWijnbergen <s1087572@student.hsleiden.nl>
2017-09-19 10:10:28 +0200

    Added the ability to select the current module-list
    A list of module-lists is retrieved from the api.
    This list is stored in a selection button. When selecting
    one of the module-lists, this list is set as the current
    module list.

--- www/client/views/Annotate.vue
+++ www/client/views/Annotate.vue
@@ -2,6 +2,9 @@
     <div>
         <div class="d-flex justify-content-between align-items-center top">
             <h3>Annotate</h3>
+            <select v-model="name" id="name-select" class="btn btn-secondary">
+                <option v-for="name in names">{{ name }}</option>
+            </select>
             <router-link to="/annotatenew" class="btn btn-primary">
                 <span class="fa fa-plus"></span>
                 New
@@ -10,3 +13,20 @@
     </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            name: null,
+            names: [],
+        }
+    },
+    created() {
+        $.getJSON(`${ROOTURL}/module-lists/`).then(data => {
+            this.names = data.names.sort((a, b) => a < b ? -1 : 1)
+            if (this.names.length > 0) this.name = this.names[0]
+            else this.$router.push('/annotatenew')
+        })
+    }
+}
+</script>
