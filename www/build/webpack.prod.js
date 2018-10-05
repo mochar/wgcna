b1e3f6515e33acabe1500475c4a2999e88e4db02
Mo Char <mocharrout@gmail.com>
2018-10-05 13:56:48 +0200

    Use cheap source map for prod

--- www/build/webpack.prod.js
+++ www/build/webpack.prod.js
@@ -19,7 +19,7 @@ if (config.electron) {
   // remove dist folder in web app mode
   exec('rm -rf dist/')
   // use source-map in web app mode
-  base.devtool = 'eval-source-map'
+  base.devtool = 'cheap-module-source-map'
 }
 
 // use hash filename to support long-term caching
