97fa9ede94b12e1c3cba2bcfa3948a38ce71d63c
Mo Char <mocharrout@gmail.com>
2018-11-10 14:27:08 +0100

    Moved analyze view to separte directory

---
+++ www/client/views/analyze/Nav.vue
@@ -0,0 +1,62 @@
+<template>
+<ul class="nav nav-tabs top position-relative">
+    <li class="nav-item">
+        <a 
+            class="nav-link" 
+            :class="{ active: tab == 'ModuleCreationTab' }" 
+            href="#" 
+            @click.prevent="$emit('update:tab', 'ModuleCreationTab')">
+            <font-awesome-icon icon="certificate" fixed-width size="sm" />
+            Generate
+        </a>
+    </li>
+    <li class="nav-item">
+        <a 
+            class="nav-link" 
+            :class="{ disabled: !hasModules, active: tab == 'ModuleInspectionTab' }" 
+            href="#" 
+            @click.prevent="$emit('update:tab', 'ModuleInspectionTab')">
+            <font-awesome-icon :icon="['far', 'eye']" fixed-width size="sm" />
+            Inspection
+        </a>
+    </li>
+    <li class="nav-item">
+        <a 
+            class="nav-link" 
+            :class="{ disabled: !hasModules, active: tab == 'ModuleSignificanceTab' }" 
+            href="#" 
+            @click.prevent="$emit('update:tab', 'ModuleSignificanceTab')">
+            <font-awesome-icon icon="chart-bar" fixed-width size="sm" />
+            Significance
+        </a>
+    </li>
+</ul>
+</template>
+
+<script>
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true
+        },
+        tab: {
+            type: String,
+            required: true
+        }
+    },
+
+    computed: {
+        hasModules() {
+            return this.project && this.project.step > 3
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.nav-link {
+    color: #999;
+}
+</style>
